1c6d99861a40267df0ad1bab0b527d07ba9ac65b
redlincon <83955984+redlincon@users.noreply.github.com>
2021-08-07 20:11:23 +0300

    Update PaymentHubMonitor.py

--- PaymentHubMonitor.py
+++ PaymentHubMonitor.py
@@ -6,7 +6,7 @@ def PaymentHubMonitor_handler(event, context):
     response = requests.get("https://dev.simusolar.lamt.app/payments/")
 
     if response.status_code == 200:
-        output = "REQUEST HAS BEEN SENT SUCCESSFULY"
+        output = "REQUEST HAS BEEN SENT SUCCESSFULLY"
         return output
 
     else:
