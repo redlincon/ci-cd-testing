280b9d131a04636cac3666d3b0532966e464eefe
redlincon <83955984+redlincon@users.noreply.github.com>
2021-08-08 14:05:29 +0300

    Editted files

--- PaymentHubMonitor.py
+++ PaymentHubMonitor.py
@@ -6,10 +6,10 @@ def PaymentHubMonitor_handler(event, context):
     response = requests.get("https://dev.simusolar.lamt.app/payments/")
 
     if response.status_code == 200:
+        email_template.send_email()
         output = "REQUEST HAS BEEN SENT SUCCESSFULLY"
         return output
 
     else:
         output = "REQUEST FAILED"
-        email_template.send_email()
         return output
--- email_template.py
+++ email_template.py
@@ -3,7 +3,7 @@ import boto3
 def send_email():
     SENDER = "PAYMENT HUB LAMBDA FUNCTION <redlincon@gmail.com>"
 
-    RECIPIENT = "hugutech@gmail.com"
+    RECIPIENT = "redlincon@gmail.com"
 
     AWS_REGION = "us-east-2"
 
